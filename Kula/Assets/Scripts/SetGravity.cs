16351a43a2d0670742db883858fd9e9efeb95821
Thomas Scaltrito <thomas.scaltrito@student.supsi.ch>
2020-10-30 13:15:03 +0100

    respawn all

---
+++ Kula/Assets/Scripts/SetGravity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGravity : MonoBehaviour
+{
+    Rigidbody body;
+
+    private bool isTouched = true;
+    private float distance = 0;
+    public Transform distanceChecker;
+    public float GroundDistance = 0.1f;
+    public LayerMask Ground;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // check if ground below character is present
+        isTouched = Physics.CheckSphere(distanceChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+
+
+
+    }
+}
